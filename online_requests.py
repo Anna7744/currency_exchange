dfe50e82d3d8d163fce80296292759619655a520
Anna7744 <49675748+Anna7744@users.noreply.github.com>
2023-09-24 00:16:04 +0500

    Create online_requests.py

---
+++ online_requests.py
@@ -0,0 +1,10 @@
+import requests
+
+URL = "https://api.freecurrencyapi.com/v1/latest?apikey="
+API_KEY = "fca_live_PanC0ghGyYnwhxk0TqFxJ8d1sOfUTRXt7rNcwIRk"
+
+
+def get_actual_currencies():
+    response = requests.get(URL + API_KEY)
+
+    return response.json()
